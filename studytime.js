cac01d1f61c76a82d35a25a39e89edcba79a3dfe
Tony Creeney <tonycreeney@gmail.com>
2023-09-28 05:37:41 +0100

    Trying to resolved the issues with closePrograms Function finishing the run before the OpenStudyPrograms function runs

--- studytime.js
+++ studytime.js
@@ -1,44 +1,54 @@
 //This script will do three things--1) get the names of the programs that are open that could distract me--battle.net, chrome, spotify, teams, etc. 2)close them all down. 3) Start up nvim, and 3 tabs in chrome--chester portal, 
 //chatgpt and google search engine.
-
-function closePrograms() {
-	const { exec } = require('child_process');
-	const programNames = ['chrome.exe', 'Spotify.exe', 'Discord.exe', 'Teams.exe', 'Battle.net.exe'];
-
-	programNames.forEach((program) => {
-		exec(`taskkill /F /IM ${program}`);
-		console.log(`${program} HAS BEEN CLOSED`);
-	});	
-};
-
+const { exec } = require('child_process');
 const puppeteer = require('puppeteer');
+
+async function closePrograms() {
+	return new Promise((resolve, reject) => {
+	const programNames = ['chrome.exe', 'Spotify.exe', 'Discord.exe', 'Teams.exe', 'Battle.net.exe'];
+	let count = 0;
+		
+	programNames.forEach((program) => {
+		exec(`taskkill /F /IM ${program}`, (error) => {
+			if (error) {
+				console.log(`Error Closing ${program}: `, error); 
+			} else {
+			console.log(`${program} HAS BEEN CLOSED`);
+			count++;
+			};
+		});
+		});
+			if (count === programNames.length) {
+				resolve();};
+	});
+}
+
 
 async function openStudyPrograms() {
 	const webpageAddress = new Map ([
-		['ChatGPT', 'https://www.chat.openai.com'],
-		['Google', 'https://www.google.com'],
+		['ChatGPT', 'http://www.openai.com'],
+		['Google', 'http://www.google.com'],
 		['Chester Portal', 'https://portal1.chester.ac.uk/Pages/default.aspx']
 	]);
 
-	 const browser = await puppeteer.launch({
-		 headless: false,
-		 executablePath: 'C:\\Users\\tosh-\\AppData\\Local\\Chromium\\Application\\chrome.exe'
-	});
-
-	 try {
-		 for (const [webpage, url] of webpageAddress) {
-		 const page = await browser.newPage();
-		 await page.goto(url);
-		 console.log(`Success! '${webpage}' Opened!`);
-		 }
-	 } catch (error) {
-		 console.log(`ERROR with ${webpage}`, error);
-		 };
+	const browser = await puppeteer.launch({ headless: false });
+	try {
+		for (const [webpage, url] of webpageAddress) {
+			const page = await browser.newPage();
+			await page.goto(url);
+			console.log(`Success!`);
+		}
+	} catch (error) {
+		console.log(`Error: `, error);
+	};
 };
-closePrograms();
 
-(async () => {
-console.log(`TEST TEST TEST ! - BEFORE`);
-await openStudyPrograms();
-console.log(`TEST TEST TEST ! - BEFORE`);
-})();
+async function main() {
+	try {
+		await closePrograms();
+		await openStudyPrograms();
+	} catch (error) {
+		console.log(`Main Error`, error)};
+};
+
+main();
